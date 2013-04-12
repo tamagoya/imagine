df1cb0289e41010522e5030f4aed0ff863232ce7
Syunsuke Mihara <mihara.git@gmail.com>
2013-04-13 00:52:24 +0900

    start imagine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imagine::Application.config.secret_token = 'b159a46e001ef02f5576ef4d8f5aa6ecbd85d23fc0c5aca6cdb9955f61074f7aea20b775690bf438256e1ba34d71e910adb70cca0909b799b9dc4a83a557b5ba'
